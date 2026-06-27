cef2f122396f0ac5daaf7f5a6036317fe1661ae2
Sujal Sevak <sujalsevak20306@gmail.com>
2026-06-27 21:15:49 +0530

    Updated Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ const Footer = () => {
             </div>
             <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
                 <p className="md:text-base text-sm md:font-normal font-light">
-                    Copyright © 2025 Sujal Sevak
+                    Copyright © {new Date().getFullYear()} Sujal Sevak
                 </p>
 
                 <div className="flex items-center md:gap-3 gap-6">
